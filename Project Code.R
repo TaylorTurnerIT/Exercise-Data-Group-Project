0dd43aff0b8251d71622d91c9f86d19a5d0a56af
William Collier <wtcollier42@tntech.edu>
2024-11-14 15:17:52 -0600

    added dplyr library
    added dplyr library

--- Project Code.R
+++ Project Code.R
@@ -1,3 +1,6 @@
+# Necessary Libraries
+library(dplyr) 
+
 # Import data set 
 workouts <- read.csv("~/Computer Science/Data Science/Project/Exercise Data Group Project/Top 50 Excerice for your body.csv")
 
