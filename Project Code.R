f42d3ebed675cbcc376facbeb69c0b8b9b0b4404
Taylor Turner <131205485+TaylorTurnerIT@users.noreply.github.com>
2024-11-14 11:07:50 -0600

    Fixed read.csv

--- Project Code.R
+++ Project Code.R
@@ -1,2 +1,2 @@
 # Import data
-data <- read.csv("~/Computer Science/Data Science/Project/Top 50 Excerice for your body.csv")
+data <- read.csv("~/Computer Science/Data Science/Project/Exercise Data Group Project/Top 50 Excerice for your body.csv")
